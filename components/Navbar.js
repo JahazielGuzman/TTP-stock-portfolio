5eaad206b4b7b1a9d70db1baf23bd75629ccadf2
JahazielGuzman <jahaziel.guzman93@gmail.com>
2020-02-17 13:41:51 -0500

    add a simple backend server file, configure nodemon to ignore .next files

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 const Navbar = () => (
     <div>
         <ul>
-            <li><Link href="/"><a>Home</a></Link></li>
-            <li><Link href="/about"><a>About</a></Link></li>
+            <li><Link href="/Portfolio"><a>Portfolio</a></Link></li>
+            <li><Link href="/Transactions"><a>Transactions</a></Link></li>
         </ul>
     </div>
 );
