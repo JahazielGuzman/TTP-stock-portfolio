0e65fb403ae87d4a58fccae5bf37b84450bd3772
JahazielGuzman <jahaziel.guzman93@gmail.com>
2020-02-18 11:58:06 -0500

    fixed closing tag in layout

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ const Layout = (props) => (
             </title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
         </Head>
         <Navbar />
         {props.children}
